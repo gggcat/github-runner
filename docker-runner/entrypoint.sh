f79501bf9a504526aef5a2b8ba1a615dad0c88ae
gggcat <61681628+gggcat@users.noreply.github.com>
2020-05-27 11:14:04 +0900

    runnerからCIFSマウントするように修正

--- docker-runner/entrypoint.sh
+++ docker-runner/entrypoint.sh
@@ -5,11 +5,21 @@ RUNNER_NAME=$2
 IS_ONLY_REGIST=$3
 CLEAN_WAIT_TIME=5.0
 
+#
+# CIFS Mount
+#
+mkdir -p ${CIFS_LOCAL_PATH}
+mount -t cifs -o username=${CIFS_USER},password=${CIFS_PASS} //${CIFS_HOST}${CIFS_REMOTE_PATH} ${CIFS_LOCAL_PATH}
+
+#
+# Run Github Self-hosted runners
+#
 echo "Runner-token url: https://api.github.com/repos/${GITHUB_USER}/${REPOSITORY_NAME}/actions/runners/registration-token"
 RUNNER_TOKEN=$(curl -s -X POST \
     -H "authorization: token ${GITHUB_TOKEN}" \
     https://api.github.com/repos/${GITHUB_USER}/${REPOSITORY_NAME}/actions/runners/registration-token | jq -r .token)
 
+echo "REGIST: https://github.com/${GITHUB_USER}/${REPOSITORY_NAME}"
 ./config.sh \
     --unattended \
     --replace \
