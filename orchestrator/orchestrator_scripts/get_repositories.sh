8a6aab7046cd6f6179aa621f2f2fe48e6cb2d942
gggcat <61681628+gggcat@users.noreply.github.com>
2021-04-17 17:00:09 +0900

    対象リポジトリ一覧取得で1つしか取れていない不具合を修正

--- orchestrator/orchestrator_scripts/get_repositories.sh
+++ orchestrator/orchestrator_scripts/get_repositories.sh
@@ -25,10 +25,11 @@ if [ -z ${GITHUB_TOKEN} ] || [ -z ${GITHUB_USER} ]; then
 fi
 
 SELF_HOSTED_REPOSITORIES="repositories.jsonl"
+rm ${SELF_HOSTED_REPOSITORIES}
 
 for REPOSITORY_NAME in $( get_repositories ); do
     IS_SELF_HOSTED=$( have_self_hosted_runner_flag_secrets ${REPOSITORY_NAME} )
     if [ "${IS_SELF_HOSTED}" == "IS_SELF_HOSTED" ]; then
-        echo "{ \"repository_name\": \"${REPOSITORY_NAME}\" }" > ${SELF_HOSTED_REPOSITORIES}
+        echo "{ \"repository_name\": \"${REPOSITORY_NAME}\" }" >> ${SELF_HOSTED_REPOSITORIES}
     fi
 done
