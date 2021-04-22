6ce63508a5d3468e1814a9b51ed7da9829cc346f
gggcat <61681628+gggcat@users.noreply.github.com>
2021-04-22 15:33:50 +0900

    スケジューリング調整

--- orchestrator/orchestrator_scripts/cronpoint.sh
+++ orchestrator/orchestrator_scripts/cronpoint.sh
@@ -10,11 +10,11 @@ sleep 2.0
 env > ${CRON_CONFIG}
 echo "" >> ${CRON_CONFIG}
 # update repositories.jsonl
-echo "0 0 * * 6 root cd /home/orchestrator && bash get_repositories.sh >> ${CRON_LOG}" >> ${CRON_CONFIG}
+echo "*/30 0 * * * root cd /home/orchestrator && bash get_repositories.sh >> ${CRON_LOG}" >> ${CRON_CONFIG}
 # clean runners
-echo "0 0 * * 6 root cd /home/orchestrator && bash clean_runner.sh >> ${CRON_LOG}" >> ${CRON_CONFIG}
+echo "*/30 0 * * * root cd /home/orchestrator && bash clean_runner.sh >> ${CRON_LOG}" >> ${CRON_CONFIG}
 # do self-hosted runners
-echo "*/10 * * * * root cd /home/orchestrator && bash orchestrator.sh >> ${CRON_LOG}" >> ${CRON_CONFIG}
+echo "*/5 * * * * root cd /home/orchestrator && bash orchestrator.sh >> ${CRON_LOG}" >> ${CRON_CONFIG}
 chmod 0644 ${CRON_CONFIG}
 crontab ${CRON_CONFIG}
 touch ${CRON_LOG}
